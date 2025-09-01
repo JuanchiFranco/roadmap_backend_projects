b8e66e41df2f6bd2bc051f4c25858b800cd0ca84
Juan Franco Vargas <francovargasjuandario@gmail.com>
2025-09-01 09:24:29 -0500

    presentation added to home

--- E-Commerce API/src/app.ts
+++ E-Commerce API/src/app.ts
@@ -1,18 +1,27 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import { errorHandler } from './middlewares/errorHandler';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 // routes
 import { authRoutes } from './components/Auth/index';
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // Middleware for parsing JSON requests
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // Route for handling GET requests to the root URL
 app.get('/', (_req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.render('home', { title: 'Home' });
 });
 
 // Use authRoutes for all routes starting with /auth
